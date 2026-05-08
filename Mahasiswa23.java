bf75a6737bd4b413a2bf5a8a4c8e7be22ba6b208
radit <pradityapramana21@gmail.com>
2026-05-08 07:56:40 +0700

    3

---
+++ Mahasiswa23.java
@@ -0,0 +1,9 @@
+class Mahasiswa23 {
+    String nim, nama, prodi, kelas;
+    Mahasiswa23(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas; }
+    void tampilData() {
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas); } }
